474d62804da5a83e2fbb1bcd6d2efa680dc6e159
Raj <rajthapar@yahoo.com>
2013-10-11 21:29:54 -0400

    2nd commit

---
+++ single_file_app.py
@@ -0,0 +1,41 @@
+from pyramid.response import Response
+from pyramid.config import Configurator
+from pyramid.view import view_config
+from wsgiref.simple_server import make_server
+from pyramid_jinja2 import _get_or_build_default_environment #?#?#?
+import json
+from pyramid.renderers import render
+#from pyramid.session import UnencryptedCookieSessionFactoryConfig
+#from pyramid.authentication import AuthTktAuthenticationPolicy
+#from pyramid.authorization import ACLAuthorizationPolicy
+ 
+ 
+@view_config(route_name='home', renderer='mytemplate.jinja2')
+def my_view(request):
+    return {'user': {'testkey1':'testvalue1', 'testkey2':'testvalue2'}}
+ 
+def json_dumps(d, request):
+    return render('json', d, request)
+
+if __name__ == '__main__':
+#    authn_policy = AuthTktAuthenticationPolicy(secret='s0secret')
+#    authz_policy = ACLAuthorizationPolicy()
+#    session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
+    config = Configurator(
+        settings={},
+#        session_factory=session_factory,
+#        authentication_policy=authn_policy,
+#        authorization_policy=authz_policy
+    )
+    config.include('pyramid_jinja2')
+    jinja_env = _get_or_build_default_environment(config.registry) #?#?#?
+    jinja_env.filters['json_dumps'] = json_dumps
+    config.add_route('home', '/')
+    config.scan('.')
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 8080, app)
+    server.serve_forever()
+
+
+
+
